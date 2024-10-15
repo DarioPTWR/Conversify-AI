c7a9dc93141d8cc09e1c0fcd35530b4f8ade2c98
Dario Prawara Teh <112337659+DarioPTWR@users.noreply.github.com>
2024-10-15 19:11:16 +0800

    fixed auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,34 +1,30 @@
-export const dynamic = 'force-dynamic';
-
-import Dashboard from '@/components/Dashboard';
-import { db } from '@/db';
+import Dashboard from '@/components/Dashboard'
+import { db } from '@/db'
 import { getUserSubscriptionPlan } from '@/lib/stripe';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { redirect } from 'next/navigation';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
 const Page = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser(); // Await the getUser() function
+  const { getUser } = getKindeServerSession()
+  const user = await getUser() // Await the getUser() function
 
   if (!user || !user.id) {
-    redirect('/auth-callback?origin=dashboard');
-    return null; // Prevent further execution
+    redirect('/auth-callback?origin=dashboard')
   }
 
   const dbUser = await db.user.findFirst({
     where: {
-      id: user.id,
-    },
-  });
+      id: (await user).id
+    }
+  })
 
   if (!dbUser) {
-    redirect('/auth-callback?origin=dashboard');
-    return null; // Prevent further execution
+    redirect('/auth-callback?origin=dashboard')
   }
 
-  const subscriptionPlan = await getUserSubscriptionPlan();
+  const subscriptionPlan = await getUserSubscriptionPlan()
+  
+  return <Dashboard subscriptionPlan={subscriptionPlan}/>
+}
 
-  return <Dashboard subscriptionPlan={subscriptionPlan} />;
-};
-
-export default Page;
+export default Page
