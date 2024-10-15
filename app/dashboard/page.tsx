7f7701d7617912ae8764040f6ebc190b03754867
Dario Prawara Teh <112337659+DarioPTWR@users.noreply.github.com>
2024-10-15 16:41:32 +0800

    fixed error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,19 +3,25 @@ import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'; // Force dynamic rendering
+
 const Page = async () => {
   const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const user = await getUser() // Await the getUser() function
 
-  if (!user || !(await user).id) redirect('/auth-callback?origin=dashboard')
+  if (!user || !user.id) {
+    redirect('/auth-callback?origin=dashboard')
+  }
 
   const dbUser = await db.user.findFirst({
     where: {
-      id: (await user).id
+      id: user.id
     }
   })
 
-  if(!dbUser) redirect('/auth-callback?origin=dashboard')
+  if (!dbUser) {
+    redirect('/auth-callback?origin=dashboard')
+  }
   
   return <Dashboard />
 }
